96d84dbd550b41e126bef92fede14a49a9ed35af
Khoa Le <ldakhoa1308@gmail.com>
2023-08-24 20:09:40 +0700

    Initial Commit

---
+++ SweepUnusedImages/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SweepUnusedImages
+//
+//  Created by Khoa Le on 24/08/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SweepUnusedImages/SweepUnusedImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweepUnusedImagesApp.swift
+//  SweepUnusedImages
+//
+//  Created by Khoa Le on 24/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct SweepUnusedImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
